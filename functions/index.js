425ec9b4c52f1bdb52ec944e095320b5c2ef7429
Khishigbayar Unursaikhan <khishigbayar@itc.gov.mn>
2020-01-27 20:03:20 +0800

    'graphql_hello'

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,45 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
+const app = express();
+
+const schema = buildSchema(`
+    type User {
+        id: ID!
+        name: String
+        age: Int
+    }
+    
+    type Query {
+        hello: String
+        user: User
+    }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello World!';
+    },
+    user: () => {
+        return {
+            id: 1,
+            name: 'Khishigbayar',
+            age: 23
+        }
+    }
+}
+
+app.use('/', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+
+
+exports.graphql = functions.https.onRequest(app);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
