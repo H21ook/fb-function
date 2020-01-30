5bb08b053ab6407406cba13953e37baa4f9f4203
Khishigbayar Unursaikhan <khishigbayar@itc.gov.mn>
2020-01-30 20:59:49 +0800

    change

--- functions/index.js
+++ functions/index.js
@@ -5,29 +5,33 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+//Firebase
+// const admin = require('firebase-admin');
+
+// admin.initializeApp();
+//Schema
 const schema = buildSchema(`
     type User {
         id: ID!
-        name: String
-        age: Int
+        name: String!
+        age: Int!
     }
-    
+
     type Query {
-        hello: String
-        user: User
+        user(id: String): User
     }
 `);
 
 const root = {
-    hello: () => {
-        return 'Hello World!';
-    },
-    user: () => {
-        return {
-            id: 1,
-            name: 'Khishigbayar',
-            age: 23
-        }
+    user: (params) => {
+        console.log(params);
+        if(params.id == 1)
+            return {
+                id: "1",
+                name: "Khishigbayar",
+                age: 23
+            };
+        else return null;
     }
 }
 
@@ -37,11 +41,33 @@ app.use('/', graphqlHTTP({
     graphiql: true
 }));
 
+    
 
+    
+
+// exports.getPosts = functions.https.onRequest((req, res) => {
+//     admin
+//     .firestore()
+//     .collection('Posts')
+//     .get()
+//     .then(posts => {
+//         return res.json(posts.docs.map((post) => post.data()));
+//     }).catch((err) => console.error(err));
+// });
+
+// exports.getUsers = functions.https.onRequest((req, res) => {
+//     admin
+//     .firestore()
+//     .collection('Users')
+//     .get()
+//     .then(users => {
+//         return res.json(users.docs.map(user => user.data()))
+//     }).catch(err => console.error(err));
+// });
 
 exports.graphql = functions.https.onRequest(app);
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
